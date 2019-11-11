96244a38f6303aa065c48f807cf0770181429c28
Cen Wang <cenwang@umass.edu>
2019-11-10 20:52:56 -0500

    Use hashed password when creating users

--- init.py
+++ init.py
@@ -1,7 +1,8 @@
 from app import db, User
+from werkzeug.security import generate_password_hash
 
 db.create_all()
 
-u = User(email="0", password="0")
+u = User(email="0", password=generate_password_hash("0"))
 db.session.add(u)
 db.session.commit()
